55f7d5c2eece9b230c2c98aae5f7b6c915c15ab5
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2022-09-28 08:51:23 +0200

    setup repoDI

---
+++ HolaMundo/src/holamundo/Nombre.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author DAM2Alu5
+ */
+public class Nombre {
+    String nombre;
+
+    public Nombre() {
+    }
+
+    public Nombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
